5d47710b642e6276e0d1c86e97ec38c0603a1404
cozmek ai <cozmekaifacilitatora@gmail.com>
2026-06-02 10:15:18 +0530

    first commit

---
+++ blog_frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: { 
+      fontFamily: {
+        bricolage: ['Bricolage Grotesque', 'sans-serif'],
+        jakarta: ['Jakarta Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
