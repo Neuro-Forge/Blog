aa3cdfae45694782f9eab3d60a70349ff7d1c3ac
cozmek ai <cozmekaifacilitatora@gmail.com>
2026-06-17 09:39:05 +0530

    cart

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,9 @@ path('products/', views.get_products.as_view(), name='product_list'),
 path('products/<int:id>/', views.get_products.as_view(), name='product_detail'),
 # path('categories/', views.get_categories, name='category_list'),
 # path('orders/', views.get_orders, name='order_list'),
+path('cart/', views.get_cart),
+path('cart/add/',views.add_to_cart),
+path('cart/remove/', views.remove_from_cart)
+
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
-from .models import category, product, userprofile, order, orderitem
+from .models import category, product, userprofile, order, orderitem, cart, cartitem
 from .serializer import categorySerializer, productSerializer, userprofileSerializer, orderSerializer, orderitemSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 # Create your views here.
 from rest_framework import status, generics, mixins
-
-
+from rest_framework.decorators import api_view
+from .serializer import cartserializer
 class get_products(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = product.objects.all()
     serializer_class = productSerializer
     lookup_field = 'id'
     
+
+@api_view(['GET'])
+def get_cart(request):
+    cart_obj, created = cart.objects.get_or_create(user=None)
+    serializer = cartserializer(cart_obj)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_to_cart(request):
+    product_id = request.data.get('product_id')
+    product_obj = product.objects.get(id=product_id)
+    
+    cart_obj, created = cart.objects.get_or_create(user=None)
+    item, created = cartitem.objects.get_or_create(cart=cart_obj, product=product_obj)
+    
+    if not created:
+        item.quantity += 1
+        item.save()
+        
+    return Response({'message': 'Product added to cart'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def remove_from_cart(request):
+    item_id = request.data.get('item_id')
+    cartitem.objects.filter(id=item_id).delete()
+    return Response({'message': 'Product removed from cart'}, status=status.HTTP_200_OK)
+  
